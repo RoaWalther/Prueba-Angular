15c58b6cfc729c249b9e2758c5ce7f18d7b882bb
Walther Roa <roawalter95@gmail.com>
2022-07-06 21:41:50 -0500

    prueba# Please enter the commit message for your changes. Lines starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba';
+  public numero: number;
+  public resultado ='';
+  
+  services: any;
+
+  public ejecutar(inputValue:number){
+    this.numero = inputValue;
+    let miarray = Array.from({length: this.numero}, () => Math.floor(Math.random() * 10));
+    let sms = 'numeros aleatorios: '+miarray;
+    alert(sms);
+    var ordenados = miarray.sort( function deMayorAMenor(elem1, elem2) {return elem1-elem2;});
+    this.imprimir(ordenados);
+  }
+  imprimir(valor){
+    let sms= 'numeros ordenados: '+ valor;
+    alert(sms);
+    this.resultado.includes(sms);
+  }
+
 }
